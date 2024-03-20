9c6b0b34d954827e40ce36eb6f7278ad92a54a8e
andrutradx <andrudeluxe@gmail.com>
2024-03-20 09:45:17 -0500

    interface segregation

--- 4-InterfaceSegregation/Tester.cs
+++ 4-InterfaceSegregation/Tester.cs
@@ -1,34 +1,24 @@
 namespace InterfaceSegregation
 {
-    public class Tester : IActivities
+  public class Tester : IWorkTeamActivities, ITestActivities
+  {
+    public Tester()
     {
-        public Tester()
-        {
-        }
-
-        public void Plan() 
-        {
-            throw new ArgumentException();
-        }
-
-        public void Comunicate() 
-        {
-            throw new ArgumentException();
-        }
-
-        public void Design() 
-        {
-            throw new ArgumentException();
-        }
-
-        public void Develop() 
-        {
-            Console.WriteLine("I'm developing the functionalities required");
-        }
-
-        public void Test() 
-        {
-            throw new ArgumentException();
-        }
     }
+
+    public void Plan()
+    {
+      throw new ArgumentException();
+    }
+
+    public void Communicate()
+    {
+      throw new ArgumentException();
+    }
+
+    public void Test()
+    {
+      throw new ArgumentException();
+    }
+  }
 }
